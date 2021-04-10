edde54ef68d70e20f0e1bbdcefd4fbc904e2b8e3
GaoangLau <ssruoz@gmail.com>
2021-04-10 23:03:28 +0800

    refactor(icwb2-data): add icwb data

---
+++ icwb2-data/benchmark.sh
@@ -0,0 +1,1 @@
+./scripts/score gold/msr_training_words.utf8 gold/msr_test_gold.utf8 result.txt > summary.txt
--- icwb2-data/hmmtrain.py
+++ icwb2-data/hmmtrain.py
@@ -1,31 +1,35 @@
 import os
 import pickle
 import math
-import json
-import operator
 from collections import defaultdict
 from dofast.utils import info
 from typing import List
 
 
 class HMM:
-    TRAIN_CORPUS = 'training/msrHMMing.utf8'
-    punctuations = set('，‘’“”。！？：')
+    TRAIN_CORPUS = 'training/msr_training.utf8'
+    TRAIN_CORPUS = 'training/pku_training.utf8'
+    punctuations = set('，‘’“”。！？：（）、')
     MIN_FLOAT = -1e10
+    emission = None
+    transition = None
 
     @classmethod
     def train(cls) -> (dict, list):
         ''' train HMM segment model from corpus '''
-        if all(os.path.exists(f) for f in ('emit_p.pickle', 'trans.pickle')):
+        if cls.emission and cls.transition:  # to avoid I/O
+            return cls.emission, cls.transition
+
+        if os.path.exists('emit_p.pickle') and os.path.exists('trans.pickle'):
             emit_p = pickle.load(open('emit_p.pickle', 'rb'))
             trans = pickle.load(open('trans.pickle', 'rb'))
+            cls.emission, cls.transition = emit_p, trans
             return emit_p, trans
 
         emit_p = defaultdict(int)
         ci = defaultdict(int)
         # b0 m1 e2 s3
         trans = [[0] * 4 for _ in range(4)]
-        _map = {'b': 0, 'm': 1, 'e': 2, 's': 3}
 
         def update_trans(i, j):
             if i < 0:
@@ -64,10 +68,7 @@ class HMM:
         info('count pairs complete.')
 
         for i, t in enumerate(trans):  # normalization
-            sum_ = sum(t)
-            trans[i] = [
-                math.log(e / sum_) if e > 0 else cls.MIN_FLOAT for e in t
-            ]
+            trans[i] = [math.log(e / sum(t)) if e > 0 else cls.MIN_FLOAT for e in t]
 
         for key, v in emit_p.items():
             emit_p[key] = math.log(v / ci[key[1]])
@@ -81,91 +82,105 @@ class HMM:
         return emit_p, trans
 
     @classmethod
-    def calculate_trans(cls,
-                        emit_p: dict,
-                        ci: dict,
-                        obs: str,
+    def calculate_trans(cls, emit_p: dict, ci: dict, obs: str,
                         state: str) -> float:
         return (1 + emit_p.get((obs, state), 0)) / ci.get(state, 1)
 
     @classmethod
-    def viterbi(cls, text: str, trans_p, emit_p):
+    def viterbi(cls, text: str, emit_p: dict, trans_p: dict) -> List[str]:
         ''' a DP method to locate the word segment scheme with maximum probability 
         for a given Chinese sentence.
-        :param text: str, observed sequences, e.g., 人性的枷锁
-        :param trans_p: dict, transition probability matrix, e.g., trans_p['B']['M'] = 0.123
+        :param text: str, observed sequence, e.g., '人性的枷锁'
         :param emit_p: dict, emission probability matrix, e.g., emit_p[('一', 'B')] = 0.0233
+        :param trans_p: dict, transition probability matrix, e.g., trans_p['B']['M'] = 0.123
         :return: list[str], word segments, e.g., ['人性', '的' ,'枷锁']
         '''
+        if not text:
+            return []
         state_index = dict(zip('BMES', range(4)))
         cache = {}
 
         for i, c in enumerate(text):
             if i == 0:
                 for s in 'BS':
-                    cache[s] = (-0.5, s)  # this inital prob is customizable
+                    cache[s] = (-0.5, s)  # this initial prob is customizable
                 cache['E'] = (cls.MIN_FLOAT, 'E')
                 cache['M'] = (cls.MIN_FLOAT, 'M')
             else:
                 cccopy = cache.copy()
                 for s in 'BMES':
-                    max_prob, _seq = float('-inf'), ''
+                    max_prob, prev_seq = float('-inf'), ''
                     for prev_state, v in cccopy.items():
                         prev_index, cur_index = state_index[prev_state], state_index[s]
 
-                        # not * but +
+                        # not '*' but '+'
                         new_prob = v[0] + trans_p[prev_index][cur_index] + emit_p.get((c, s), cls.MIN_FLOAT)
                         if new_prob > max_prob:
                             max_prob = new_prob
-                            _seq = v[1]
-                    cache[s] = (max_prob, _seq + '->' + s)
+                            prev_seq = v[1]
+                    cache[s] = (max_prob, prev_seq + '->' + s)
 
+        # assume a sentence ends with either 'E' or 'S'
+        # print(cache)
         seq = cache['E'][1] if cache['E'][0] > cache['S'][0] else cache['S'][1]
-        print(cls._cut(text, seq))
+        return cls._cut(text, seq)
 
     @classmethod
-    def _cut(cls, text: str, seq: str) -> str:
-        seq = seq.split('->')
-        res = ''
-        for a, b in zip(text, seq):
-            if (b == 'B' or b == 'S') and res:
-                res += ' '
-            res += a
+    def _cut(cls, text: str, seq: str) -> List[str]:
+        ''' seperate a sequence by word lexeme.
+        e.g., input ('人性的枷锁', 'B->E->S->B->E'), output ['人性','的','枷锁']
+        '''
+        res = []
+        # print(seq, text)
+        # print(len(text))
+        for a, b in zip(text, seq.split('->')):
+            if (b == 'B' or b == 'S'):
+                res.append('')
+            res[-1] += a
+            # print(res)
+
         return res
 
     @classmethod
     def segment(cls, text: str) -> List[str]:
-        res = []
+        emit_p, trans = cls.train()
+        res, prev_text = [], ''
         for i, c in enumerate(text):
             if c in cls.punctuations:
-                # res += cls.viterbi(states, text, start_p, trans, emit_p)
-                pass
+                res += cls.viterbi(prev_text, emit_p, trans)
+                res.append(c)
+                prev_text = ''
             else:
-                pass
+                prev_text += c
+
+        if prev_text:
+            res += cls.viterbi(prev_text, emit_p, trans)
+        return res
 
 
-emit_p, trans = HMM.train()
-start_p = [-0.6931471805599453, 0, 0, -0.6931471805599453]
-
-cnt = 0
-for k, v in emit_p.items():
-    print(k, v)
-    if cnt > 10:
-        break
-    cnt += 1
-
-print('-' * 80)
-cs = defaultdict(dict)
-for k, v in emit_p.items():
-    if k[0] == '我':
-        print(k, v)
-
-print("\n\n")
 texts = [
+    # '看了你的信，我被你信中流露出的凄苦、
+    '看了你的信，我被你信中流露出的凄苦、迷惘以及热切求助的情绪触动了。',
     '这是一种基于统计的分词方案', '这位先生您手机欠费了', '还有没有更快的方法', '买水果然后来世博园最后去世博会',
     '欢迎新老师生前来就餐', '北京大学生前来应聘', '今天天气不错哦', '就问你服不服', '我们不只在和你们一家公司对接',
     '结婚的和尚未结婚的都沿海边去了', '这也许就是一代人的命运吧', '改判被告人死刑立即执行', '检察院鲍绍坤检察长',
     '腾讯和阿里都在新零售大举布局', '人性的枷锁'
 ]
+
+texts += [
+    '好的，现在我们尝试一下带标点符号的分词效果。', '中华人民共和国不可分割，坚决捍卫我国领土。', '英国白金汉宫发表声明，菲利浦亲王去世。', '扬帆远东做与中国合作的先行'
+]
+
+texts=[
+    '瓦西里斯的船只中有４０％驶向远东，每个月几乎都有两三条船停靠中国港口。'
+    , '他感受到了中国经济发展的大潮'
+]
 for text in texts:
-    HMM.viterbi(text, trans, emit_p)
+    ret = ' '.join(HMM.segment(text))
+    print(ret)
+
+# with open('testing/msr_test.utf8', 'r') as f:
+#     for ln in f.readlines():
+#         ln = ln.strip()
+#         ret = ' '.join(HMM.segment(ln))
+#         print(ret)
